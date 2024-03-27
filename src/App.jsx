67a2125fbc1e0558e13fe9b6d5add636ff913ad8
Vaibhav Kumar Jha <vaibhavkumarjha46@gmail.com>
2024-03-27 12:06:17 +0530

    Adding Project SideBars

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import ProjectSidebar from "./components/ProjectSidebar";
+
 function App() {
   return (
     <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+      <main>
+        <ProjectSidebar />
+      </main>
     </>
   );
 }
