742ba7ce7634feda822d16e106d6f16e6cbbae42
Vaibhav Kumar Jha <vaibhavkumarjha46@gmail.com>
2024-03-28 18:08:51 +0530

    Splitting Components to Split Jsx & Tailwind Styles

---
+++ src/components/NoProjectSelected.jsx
@@ -0,0 +1,14 @@
+import Button from './Button';
+import noProjectImage from '../assets/no-projects.png'
+export default function NoProjectSelected() {
+  return (
+    <div className="mt-24 text-center w-2/3">
+      <img src={noProjectImage} alt='empty Task List' className='w-16 h-16 object-contain mx-auto'/>
+      <h2 className='text-xl font-bold text-tone-500 my-4'>No Project Selected</h2>
+      <p className=' text-stone-400 mb-4'>Select a Project or get started with a new one</p>
+      <p className='mt-8'>
+        <Button>Create new Project</Button>
+      </p>
+    </div>
+  );
+}
